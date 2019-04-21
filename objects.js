b9f0bbad9e45c404f1cb0f9d37a46a2cfcd01019
James Powell <nvrquitjp@gmail.com>
2019-04-21 19:39:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -64,12 +64,31 @@ meals.breakfast = 'cereal'
 function dog(obj, key, value) {
   
   obj[key] = value
+  
+  return obj
 }
 
+const recipe = {eggs: 3}
 
+cooking(recipe, 'flour','3 cups')
 
+Objec.assign( {}, {foo: 'bar' })
 
+Object.assign({ eggs: 3 }, {
+  flour: '1 cup'
+})
 
+Object.assign({ eggs: 3 }, {
+  chocolate: '1 cup', flour: '2 cups'
+} , { flour: '1/2 cup'})
+
+function coomar(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function goer(targetObject, updateObject) {
+  return Object.assign({}, targetObject, updateObject)
+}
 
 
 
