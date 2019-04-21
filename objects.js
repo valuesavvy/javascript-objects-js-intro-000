c24c3483632d66a077a1e282bd7a65f973b3af41
James Powell <nvrquitjp@gmail.com>
2019-04-21 19:58:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   artistName: 'song title'
 }
 
-function updatePlaylist(playlist, artistName, song title) {
- return Object.assign({}, playlist, { [artistName]: song title })
+function updatePlaylist(playlist, artistName, songtitle) {
+ return Object.assign({}, playlist, { [artistName]: songtitle })
 }
 
