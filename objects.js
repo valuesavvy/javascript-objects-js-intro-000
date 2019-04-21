b6cefb974bc156a30801913ae315611c8cdea1d7
James Powell <nvrquitjp@gmail.com>
2019-04-21 19:27:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,83 @@
-var playlist =  {
-  John_Legend: 'ood morning',
-  Xavier: 'Blind man'
+// var playlist =  {
+//   John_Legend: 'ood morning',
+//   Xavier: 'Blind man'
+// };
+
+var meals = {};
+
+var meals = new Object ();
+
+var meals = {
+    breakfast: "oatmeal"
 };
 
-var updatePlayList (playlist, artistName, songTitle) {
-  
-  Object.assign ({},
-   playlist, { [george]: 'Fish'});
+var meals = new Object ({
+  breakfast: 'oatmeal'
+})
+
+var meals = {
+  breakfast: 'avocado',
+  lunch: 'avocado',
+  dinner: 'avocado'
 }
+
+const firstMeal = 'breakfast'
+var meals = {firstMeal: 'oatmeal'}
+
+var meals = { [firstMeal]: 'oatmeal' }
+
+meals.breakfast
+
+meals['breakfast']
+
+meals[firstMeal]
+
+meals.firstMeal
+
+var meals = {
+  breakfast: 'oatmeal',
+  lunch: 'burrito',
+  dinner: 'steak'
+}
+
+meals.snack = 'yogurt';
+
+meals.snack
+
+meals['snack']
+
+meals.lunch
+
+
+meals['second breakfast'] = 'bagel'
+
+var sweetMeal = 'dessert'
+
+meals[sweetMeal] = 'cake';
+
+meals.dessert
+
+meals[sweetMeal]
+
+meals.breakfast = 'cereal'
+
+function dog(obj, key, value) {
+  
+  obj[key] = value
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
