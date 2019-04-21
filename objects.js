ffd42c1cfa4ffd333b7d3bf31c0be2cb703531d7
James Powell <nvrquitjp@gmail.com>
2019-04-21 20:00:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,6 @@ function updatePlaylist(playlist, artistName, songtitle) {
  return Object.assign({}, playlist, { [artistName]: songtitle })
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
