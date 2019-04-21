47fc676050ad3242cd015f68d3942b4108e29ec6
James Powell <nvrquitjp@gmail.com>
2019-04-21 19:07:45 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist =  {
+  John_Legend: 'ood morning',
+  Xavier: 'Blind man'
+};
+
+var updatePlayList (playlist, artistName, songTitle) {
+  
+  Object.assign ({},
+   playlist, { [george]: 'Fish'});
+}
